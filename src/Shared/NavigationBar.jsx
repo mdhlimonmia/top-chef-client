b9367af86ae639c15c2372e5e05df2340d42458d
Mdhlimon <mdhlimonmia@gmail.com>
2023-05-05 17:59:38 +0600

    Term and condition added

--- src/Shared/NavigationBar.jsx
+++ src/Shared/NavigationBar.jsx
@@ -1,18 +1,43 @@
-import React from "react";
-import { Navbar } from "react-bootstrap";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
+import React, { useContext } from "react";
+import { Button, Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import { FaRegUserCircle, FaUserCircle } from 'react-icons/fa';
+import { Link } from "react-router-dom";
+import { AuthContext } from "../providers/AuthProvider";
+
 const NavigationBar = () => {
+  const { user, logOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    logOut()
+      .then()
+      .catch((error) => console.log(error));
+  };
   return (
     <div>
-     <Navbar bg="primary" variant="dark">
+      <Navbar bg="primary" variant="dark">
         <Container>
           <Navbar.Brand href="/">TOP CHEFS</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/blogs">Blog</Nav.Link>
             <Nav.Link href="#pricing">About</Nav.Link>
-            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav>
+              {/* {user && (
+                <FaRegUserCircle style={{ fontSize: "2rem" }}></FaUserCircle>
+              )} */}
+
+              {user ? (
+                <Button onClick={handleLogOut} variant="secondary">
+                  Logout
+                </Button>
+              ) : (
+                <Link to="/login">
+                  <Button variant="secondary">Login</Button>
+                </Link>
+              )}
+            </Nav>
             <Nav.Link href="/register">Register</Nav.Link>
           </Nav>
         </Container>
--- src/Shared/Terms/Terms.jsx
+++ src/Shared/Terms/Terms.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Terms = () => {
     return (
-        <div>
+        <Container>
             <h2>Terms and conditions</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus ratione veritatis hic perferendis, mollitia non nostrum molestiae facere possimus autem fuga corrupti omnis rem voluptates veniam accusantium vero repudiandae ipsa.</p>
+            <p>You can't  west food. You can't say bad any thing about chefs and food.</p>
             <p>Go back to <Link to="/register">Register</Link> </p>
-        </div>
+        </Container>
     );
 };
 
