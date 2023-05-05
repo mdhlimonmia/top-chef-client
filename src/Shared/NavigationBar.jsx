40092031364691ffc541e5c7be1568ff8ef3a3d5
Mdhlimon <mdhlimonmia@gmail.com>
2023-05-05 18:24:26 +0600

    Navbar responsive done

--- src/Shared/NavigationBar.jsx
+++ src/Shared/NavigationBar.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Button, Navbar } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
-import { FaRegUserCircle, FaUserCircle } from 'react-icons/fa';
+import { FaRegUserCircle, FaUserCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 
@@ -16,18 +16,22 @@ const NavigationBar = () => {
   };
   return (
     <div>
-      <Navbar bg="primary" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
         <Container>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
           <Navbar.Brand href="/">TOP CHEFS</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/blogs">Blog</Nav.Link>
             <Nav.Link href="#pricing">About</Nav.Link>
             <Nav>
-              {/* {user && (
-                <FaRegUserCircle style={{ fontSize: "2rem" }}></FaUserCircle>
-              )} */}
-
+              {user && (
+                <FaRegUserCircle style={{ fontSize: "2rem" }}>
+                  {" "}
+                </FaRegUserCircle>
+              )}
+              ,
               {user ? (
                 <Button onClick={handleLogOut} variant="secondary">
                   Logout
@@ -37,9 +41,14 @@ const NavigationBar = () => {
                   <Button variant="secondary">Login</Button>
                 </Link>
               )}
+              ,
+              {!user && (
+                <Nav.Link href="/register">Register</Nav.Link>
+              )},
             </Nav>
-            <Nav.Link href="/register">Register</Nav.Link>
+            
           </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
