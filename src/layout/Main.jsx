7713096d727d95280db9b59d8feda5a3905b6b12
Mdhlimon <mdhlimonmia@gmail.com>
2023-05-05 19:26:21 +0600

    footer done

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavigationBar from '../Shared/NavigationBar';
+import Footer from '../pages/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <NavigationBar></NavigationBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/pages/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <div className="bg-primary">
+      <div className="card text-center bg-dark text-light">
+        <div className="card-header">Top Chefs</div>
+        <Row>
+          <Col>
+            <div>
+              <h5 className="card-title">About</h5>
+              <p className="card-text">
+                We provide Top Chefs and best recipes. You can find here your
+                choices.
+              </p>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <h5 className="card-title">Our best Chefs</h5>
+              <p>Anne-Sophie</p>
+              <p>Mario Batali</p>
+              <p>Aliya LeeKong</p>
+              <p>Eric Ripert</p>
+              <p>William Bradley</p>
+              <p>Thomas Keller</p>
+            </div>
+          </Col>
+          <Col>
+            <div>
+              <h5 className="card-title">Contact</h5>
+              <p>787 Mark View Street, New Town, California</p>
+              <p>topchefs@thatix.com</p>
+              <p>666 888 0000</p>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
